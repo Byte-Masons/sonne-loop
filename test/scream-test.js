edb97b15a4795d7e973a71d5a0e50af9ee9b41ee
imrtlfarm <imrtlfarm@gmail.com>
2022-10-04 18:30:44 -0400

    change test name

--- test/scream-test.js
+++ test/scream-test.js
@@ -102,7 +102,7 @@ describe('Vaults', function () {
     console.log('treasury');
     want = await Want.attach(wantAddress);
     console.log('want attached');
-    vault = await Vault.deploy(wantAddress, 'DAI Sonne Crypt', 'rf-soDAI', 0, ethers.constants.MaxUint256);
+    vault = await Vault.deploy(wantAddress, 'sUSD Sonne Crypt', 'rf-soSUSD', 0, ethers.constants.MaxUint256);
     console.log('vault');
 
     console.log(`vault.address: ${vault.address}`);
