8726727972feb07a70b48a2cbe0b2062ac2ba8d1
Magnus Brantheim <m.brantheim@gmail.com>
2022-10-05 10:55:00 +0200

    Deploy USDC

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0xA75a9af9626Bb6eb2684Fc5b5a2348CeBb89a1dA';
+  const strategyAddress = '0xE095a5B61B6Cc27eD755dAE84884914133c7cB8B';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
