792e1627284b353e4247a01bd57b19f8bf458a46
Magnus Brantheim <m.brantheim@gmail.com>
2022-10-05 10:10:02 +0200

    Update deploy scripts

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,8 +1,8 @@
 async function main() {
-  const vaultAddress = '0xC2cE269f3646a5F5bF1cCDa73c6cAB50f64012b6';
-  const strategyAddress = '0xb2C1De1f7f03054BDCbeC35f9f8370C09719fF1D';
+  const vaultAddress = '';
+  const strategyAddress = '';
 
-  const Vault = await ethers.getContractFactory('ReaperVaultv1_3');
+  const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
 
   //const options = { gasPrice: 2000000000000, gasLimit: 9000000 };
